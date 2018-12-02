b565844bda55af791d02b3294bb844df96a0aba8
Ivan Petrov <ivan.antonov.p@gmail.com>
2018-12-02 13:03:30 +0200

    better request validation and error handling

--- src/index.js
+++ src/index.js
@@ -3,59 +3,49 @@
 const app = require('express')();
 const config = require('config').get('AppConfig');
 const bodyParser = require('body-parser');
+const isUUID = require('validator/lib/isUUID');
 
 const authHandlers  =  require('./routes/authentication');
 const userHandlers = require('./routes/user');
 const helpers = require('./helpers');
 
 module.exports = function() {
-    
     app.use(bodyParser.json());
 
-    app.get('/', async (req, res, next) => {
-        try {
-            await authHandlers.handleGetRoot(req, res);
-        } catch (err) {
-            res.status(501).send({ error: 'Internal Server Error' });
+    app.all('/user/*', (req, res, next) => {
+        let userId = req.query.userId;
+        if (!userId) {
+            return res.status(400).send({ status: 'failed', error: 'userId query parameter required'});
+        } else if (!isUUID(userId)) {
+            return res.status(400).send({ status: 'failed', error: 'userId should be a valid UUID'});
+        } else {
+            next();
         }
+    })
+
+    app.get('/', (req, res) => {
+        return authHandlers.handleGetRoot(req, res);
     })
     
-    app.get('/callback', async (req, res, next) => {
-        try {
-            let userId =  await authHandlers.handleCallback(req, res);
-            res.send({ status: 'success', userId: userId });
-        } catch (err) {
-            res.status(501).send({ error: 'Internal Server Error' });
-        }
+    app.get('/callback', async (req, res) => {
+        return res.send({ status: 'success', result: { userId: await authHandlers.handleCallback(req, res) } });
     })
 
-    app.get('/user/transactions', async (req, res, next) => {
-        let userId = req.query.userId;
-        if(!userId) { 
-            next({ status: 'failed', error: 'userId query parameter missing'});
-        }
-
-        try {
-            let transactions = await userHandlers.fetchUserTransactions(userId);
-            res.send(transactions);
-        } catch (err) {
-            res.status(501).send({ error: 'Internal Server Error' });
-            
-        }
+    app.get('/user/transactions', async (req, res) => {
+        return res.send({ status: 'success', result: { transactions: await userHandlers.fetchUserTransactions(req.query.userId)}});
     })
 
-    app.get('/user/debug', async (req, res, next) => {
-        let userId = req.query.userId;
-        if(!userId) { 
-            next({ status: 'failed', error: 'userId query parameter missing'});
-        }
+    app.get('/user/debug', async (req, res) => {
+        return res.send({ status: 'success', result: { debugInformation: await helpers.fetchDebugInformation(req.query.userId)}});
+    })
 
-        try {
-            let debugInformation = await helpers.fetchDebugInformation(userId);
-            res.send(debugInformation);
-        } catch (err) {
-            res.status(501).send({ error: 'Internal Server Error' });
-        }
+    app.use('/*', async (req, res, next) => {
+        console.log(JSON.stringify(req, null, 4))
+        next()
+    })
+
+    app.use((err, req, res, next) => {
+        return res.status(500).send({ status: 'failed', error: 'Internal Server Error' })
     })
     
     app.listen(config.port, () => {
--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -10,22 +10,18 @@ const handleGetRoot = async (req, res) => {
 }
 
 const handleCallback = async (req, res) => {
-  try {
-    let tokens = await TrueLayerController.exchangeCodeForTokens(redirectUrl, req.query.code);
-    let userId = await UserController.storeUser(tokens);
+  let tokens = await TrueLayerController.exchangeCodeForTokens(redirectUrl, req.query.code);
+  let userId = await UserController.storeUser(tokens);
 
-    let accessToken = tokens.accessToken;
+  let accessToken = tokens.accessToken;
 
-    // fetch and store bank transactions
-    fetchTransactions(TrueLayerController.fetchAccounts, TrueLayerController.fetchBankTransactions, userId, accessToken, 'bank');
+  // fetch and store bank transactions
+  fetchTransactions(TrueLayerController.fetchAccounts, TrueLayerController.fetchBankTransactions, userId, accessToken, 'bank');
 
-    // fetch and store card transactions
-    fetchTransactions(TrueLayerController.fetchCards, TrueLayerController.fetchCardTransactions, userId, accessToken, 'card');
-        
-    return res.status(200).send({status: 'success', userId});
-  } catch (err) {
-    return res.status(500).send(JSON.stringify(err, null, 2));
-  }
+  // fetch and store card transactions
+  fetchTransactions(TrueLayerController.fetchCards, TrueLayerController.fetchCardTransactions, userId, accessToken, 'card');
+      
+  return userId;
 }
 
 module.exports = {
